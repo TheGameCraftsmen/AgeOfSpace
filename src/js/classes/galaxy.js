42ecdf3c538d5894abfe8ec57ee60ff843197b83
azziliz <chriskang@free.fr>
2017-09-09 00:30:30 +0200

    proto

--- src/js/classes/galaxy.js
+++ src/js/classes/galaxy.js
@@ -1,1 +1,27 @@
-﻿
+﻿/**
+ * @file Galaxy class. Part of the "Age of Space" project.
+ * @author github.com/azziliz
+ * @author github.com/thyshimrod
+ * {@link https://github.com/The-game-craftmen/age-of-space/ Project page}
+ * @license MIT
+ */
+
+'use strict';
+//debugger;
+
+/**
+ * The galaxy is the largest renderable game object. It contains stars that can be zoomed-in.
+ *
+ * @class
+ */
+aos.Galaxy = function () {
+    /** @type {number} */
+    this.size = 0;
+};
+
+aos.Galaxy.prototype = {
+
+    generate: function () {
+    },
+
+};
