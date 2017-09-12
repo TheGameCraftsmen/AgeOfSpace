2eac47e9514d0eb33de6a760b6b8ab886c6d1d1e
azziliz <chriskang@free.fr>
2017-09-12 23:34:56 +0200

    star naming

--- src/js/classes/galaxy.js
+++ src/js/classes/galaxy.js
@@ -325,6 +325,32 @@ aos.Galaxy.prototype = {
 { id: 87, name: 'Volans', abbr: 'Vol', gen: 'Volantis' },
 { id: 88, name: 'Vulpecula', abbr: 'Vul', gen: 'Vulpeculae' }
         ];
+        const greekLetters = [
+{ id: 1, maj: 'Α', min: 'α', name: 'alpha' },
+{ id: 2, maj: 'Β', min: 'β', name: 'bêta' },
+{ id: 3, maj: 'Γ', min: 'γ', name: 'gamma' },
+{ id: 4, maj: 'Δ', min: 'δ', name: 'delta' },
+{ id: 5, maj: 'Ε', min: 'ε', name: 'epsilon' },
+{ id: 6, maj: 'Ζ', min: 'ζ', name: 'zêta' },
+{ id: 7, maj: 'Η', min: 'η', name: 'êta' },
+{ id: 8, maj: 'Θ', min: 'θ', name: 'thêta' },
+{ id: 9, maj: 'Ι', min: 'ι', name: 'iota' },
+{ id: 10, maj: 'Κ', min: 'κ', name: 'kappa' },
+{ id: 11, maj: 'Λ', min: 'λ', name: 'lambda' },
+{ id: 12, maj: 'Μ', min: 'μ', name: 'mu' },
+{ id: 13, maj: 'Ν', min: 'ν', name: 'nu' },
+{ id: 14, maj: 'Ξ', min: 'ξ', name: 'ksi/xi' },
+{ id: 15, maj: 'Ο', min: 'ο', name: 'omicron' },
+{ id: 16, maj: 'Π', min: 'π', name: 'pi' },
+{ id: 17, maj: 'Ρ', min: 'ρ', name: 'rhô' },
+{ id: 18, maj: 'Σ', min: 'σ', name: 'sigma' },
+{ id: 19, maj: 'Τ', min: 'τ', name: 'tau' },
+{ id: 20, maj: 'Υ', min: 'υ', name: 'upsilon' },
+{ id: 21, maj: 'Φ', min: 'ϕ', name: 'phi' },
+{ id: 22, maj: 'Χ', min: 'χ', name: 'khi/chi' },
+{ id: 23, maj: 'Ψ', min: 'ψ', name: 'psi' },
+{ id: 24, maj: 'Ω', min: 'ω', name: 'oméga' },
+        ];
         constellationReference.forEach(function (c) {
             c.rng = Math.random();
         }, this);
@@ -336,6 +362,17 @@ aos.Galaxy.prototype = {
             c.reference = constellationReference[i];
             c.computeEdges();
             c.kruskal();
+            c.starCoordinates.forEach(function (star) {
+                star.group = Math.random();
+            }, this);
+            const sortedStars = c.starCoordinates.slice()
+            sortedStars.sort(function (a, b) {
+                return a.group - b.group;
+            });
+            sortedStars.forEach(function (star, i) {
+                //star.greek = greekLetters[i].name.charAt(0).toUpperCase() + greekLetters[i].name.slice(1);
+                star.greek = greekLetters[i];
+            }, this);
             c.render(false);
         }, this);
 
@@ -380,41 +417,41 @@ aos.Galaxy.prototype = {
             document.getElementById('starOverlay').style.cursor = 'default';
             document.getElementById('starSystemBlock').style.display = 'none';
 
-            let closeStar = false;
-
-            if (!closeStar) {
-                const canvas = document.getElementById('starOverlay');
-                const ctx = canvas.getContext('2d');
-                ctx.clearRect(0, 0, 1200, 900);
-                const constellationId = instance.getConstellationId(galaxyCoordX, galaxyCoordY);
-                instance.constellations.forEach(function (c, i) {
-                    c.render(i === constellationId);
-                });
-                if (constellationId === 0) {
-                    ctx.beginPath();
-                    ctx.strokeStyle = '#c00';
-                    ctx.lineWidth = 2;
-                    ctx.arc(600, 450, 50, 0, 2 * Math.PI);
-                    ctx.stroke();
-                    document.getElementById('starSystemBlock').style.display = 'block';
-                    document.getElementById('starSystemName').innerHTML = 'Galactic Core';
-                }
-                else {
-                    document.getElementById('starSystemBlock').style.display = 'block';
-                    document.getElementById('starSystemName').innerHTML = '' + instance.constellations[constellationId].reference.name;
-                }
+            const canvas = document.getElementById('starOverlay');
+            const ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, 1200, 900);
+            const constellationId = instance.getConstellationId(galaxyCoordX, galaxyCoordY);
+            instance.constellations.forEach(function (c, i) {
+                c.render(i === constellationId);
+            });
+            if (constellationId === 0) {
+                ctx.beginPath();
+                ctx.strokeStyle = '#c00';
+                ctx.lineWidth = 2;
+                ctx.arc(600, 450, 50, 0, 2 * Math.PI);
+                ctx.stroke();
+                document.getElementById('starSystemBlock').style.display = 'block';
+                document.getElementById('starSystemName').innerHTML = 'Galactic Core';
             }
+            else {
+                document.getElementById('starSystemBlock').style.display = 'block';
+                document.getElementById('starSystemName').innerHTML = '' + instance.constellations[constellationId].reference.name
+                + '<br>Constellation';
+            }
+
             //document.getElementById('stats').innerHTML = '' + constellationId + '/' + '<br/>';
 
-            instance.notableStarCoordinates.forEach(function (star) {
+            instance.constellations[constellationId].starCoordinates.forEach(function (star) {
                 const deltaX = star.x - galaxyCoordX;
                 const deltaY = star.y - galaxyCoordY;
                 const dist = deltaX * deltaX + deltaY * deltaY;
-                if (dist < 400) { // sqrt(dist) < 20.0
-                    closeStar = true;
+                const radius = star.notable ? 400.0 : 100.0;
+                if (dist < radius) { // sqrt(dist) < 20.0
                     document.getElementById('starOverlay').style.cursor = 'pointer';
                     document.getElementById('starSystemBlock').style.display = 'block';
-                    document.getElementById('starSystemName').innerHTML = 'Alpha Centauri';
+                    document.getElementById('starSystemName').innerHTML = '' +
+                        star.greek.name.charAt(0).toUpperCase() + star.greek.name.slice(1) + ' ' +
+                        instance.constellations[constellationId].reference.gen;
                 }
                 //document.getElementById('stats').innerHTML += dist + '<br/>';
             });
--- src/js/classes/star.js
+++ src/js/classes/star.js
@@ -27,32 +27,6 @@ aos.Star.prototype = {
     },
 
     generateName: function () {
-        const greekLetters = [
-{ id: 1, maj: 'Α', min: 'α', name: 'alpha' },
-{ id: 2, maj: 'Β', min: 'β', name: 'bêta' },
-{ id: 3, maj: 'Γ', min: 'γ', name: 'gamma' },
-{ id: 4, maj: 'Δ', min: 'δ', name: 'delta' },
-{ id: 5, maj: 'Ε', min: 'ε', name: 'epsilon' },
-{ id: 6, maj: 'Ζ', min: 'ζ', name: 'zêta' },
-{ id: 7, maj: 'Η', min: 'η', name: 'êta' },
-{ id: 8, maj: 'Θ', min: 'θ', name: 'thêta' },
-{ id: 9, maj: 'Ι', min: 'ι', name: 'iota' },
-{ id: 10, maj: 'Κ', min: 'κ', name: 'kappa' },
-{ id: 11, maj: 'Λ', min: 'λ', name: 'lambda' },
-{ id: 12, maj: 'Μ', min: 'μ', name: 'mu' },
-{ id: 13, maj: 'Ν', min: 'ν', name: 'nu' },
-{ id: 14, maj: 'Ξ', min: 'ξ', name: 'ksi/xi' },
-{ id: 15, maj: 'Ο', min: 'ο', name: 'omicron' },
-{ id: 16, maj: 'Π', min: 'π', name: 'pi' },
-{ id: 17, maj: 'Ρ', min: 'ρ', name: 'rhô' },
-{ id: 18, maj: 'Σ', min: 'σ', name: 'sigma' },
-{ id: 19, maj: 'Τ', min: 'τ', name: 'tau' },
-{ id: 20, maj: 'Υ', min: 'υ', name: 'upsilon' },
-{ id: 21, maj: 'Φ', min: 'ϕ', name: 'phi' },
-{ id: 22, maj: 'Χ', min: 'χ', name: 'khi/chi' },
-{ id: 23, maj: 'Ψ', min: 'ψ', name: 'psi' },
-{ id: 24, maj: 'Ω', min: 'ω', name: 'oméga' },
-        ];
 
     }
 };
